f79df517d4bda5c52c449e04b63e39eacb4a55c7
Jonathan Huertas <jhuertas@qstruct.ecn.purdue.edu>
2024-11-24 17:22:18 -0500

    Adding in changes to CVU header and cc files to include new methods, values being passed

--- src/cpu/minor/CVU.cc
+++ src/cpu/minor/CVU.cc
@@ -1,3 +1,34 @@
 #include "cpu/minor/CVU.hh"
 
-CVU::CVU() {}
+CVU::CVU(size_t size) : maxSize(size) {}
+
+//look for an entry
+bool CVU::lookup(uint32_t index, uint32_t &value) {
+    auto it = cvuTable.find(index);
+    if(it != cvuTable.end()) {
+        return true;
+    }
+    return false;
+}
+
+//insert an entry
+void CVU::insert(uint32_t index, uint32_t value) {
+    //check if the table size is at maxSize -> if it is need to evict something
+    evictIfFull();
+    cvuTable[index] = {index, value}; // push in
+}
+
+//erase an entry
+void CVU::invalidate(uint32_t index) {
+    auto it = cvuTable.find(index);
+    if (it != cvuTable.end()) {
+        cvuTable.erase(it);
+    }
+}
+
+//evict from table
+void CVU::evictIfFull() {
+    if (cvuTable.size() >= maxSize) {
+        cvuTable.erase(cvuTable.begin()); //remove first entry for simplicity 
+    }
+}
--- src/cpu/minor/CVU.hh
+++ src/cpu/minor/CVU.hh
@@ -7,20 +7,21 @@
 class CVU : public Named {
 private:
     struct CVUEntry {
-       
-       uint32_t value;
+       uint32_t index; // index coming from lvpt
+       uint32_t value; // value of the constant load
     };
 
-    std::unordered_map<Addr, CVUEntry> cvuTable; // the main cvu table
+    std::unordered_map<uint32_t, CVUEntry> cvuTable; // the main cvu table
     size_t maxSize; // max cvu size
+    void evictIfFull();
 public:
     CVU(size_t size);
 
     //function to check if value is in cvu table;
-
+    bool lookup(uint32_t index, uint32_t &value);
     // insert a new constant value in cvu table;
-
+    void insertValue(uint32_t index, uint32_t value);
     // invalidate or remove entry from table by its address
-
+    void invalidate(uint32_t index);
 };
 #endif // __CVU_HH__
