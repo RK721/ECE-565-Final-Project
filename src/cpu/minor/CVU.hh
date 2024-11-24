278e4d73340770bbc1671e05112d877bbee9fc7a
Jonathan Huertas <jhuertas@qstruct.ecn.purdue.edu>
2024-11-24 16:35:19 -0500

    Adding in values and pseudo code for methods that will be needed

--- src/cpu/minor/CVU.hh
+++ src/cpu/minor/CVU.hh
@@ -1,16 +1,26 @@
 #ifndef __CVU_HH__
 #define __CVU_HH__
 
-#include "base/types.hh"
+#include <unordered_map>
+#include "base/named.hh"
 
-class CVU {
+class CVU : public Named {
 private:
     struct CVUEntry {
        
+       uint32_t value;
     };
 
+    std::unordered_map<Addr, CVUEntry> cvuTable; // the main cvu table
+    size_t maxSize; // max cvu size
 public:
-    CVU();
+    CVU(size_t size);
+
+    //function to check if value is in cvu table;
+
+    // insert a new constant value in cvu table;
+
+    // invalidate or remove entry from table by its address
 
 };
-#endif // CVU_HH
+#endif // __CVU_HH__
